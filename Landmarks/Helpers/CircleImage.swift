3f493e2ee178d4a57391e8d06645f96f41488a22
laure leter <laure.leter@gmail.com>
2021-12-08 13:34:28 +0100

    first commit

---
+++ Landmarks/Helpers/CircleImage.swift
@@ -0,0 +1,35 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Laure Leter on 07/12/2021.
+//
+
+import SwiftUI
+
+
+struct CircleImage: View {
+    
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+                    }
+            .shadow(radius: 7)
+        }
+
+}
+
+
+struct CircleImage_Previews: PreviewProvider {
+
+    static var previews: some View {
+
+        CircleImage(image: Image("turtlerock"))
+
+    }
+
+}
---
+++ Landmarks/Helpers/FavoriteButton.swift
@@ -0,0 +1,45 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Laure Leter on 08/12/2021.
+//
+
+import SwiftUI
+
+
+struct FavoriteButton: View {
+
+    @Binding var isSet: Bool
+
+
+    var body: some View {
+
+        Button {
+
+            isSet.toggle()
+
+        } label: {
+
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+
+                .labelStyle(.iconOnly)
+
+                .foregroundColor(isSet ? .yellow : .gray)
+
+        }
+
+    }
+
+}
+
+
+struct FavoriteButton_Previews: PreviewProvider {
+
+    static var previews: some View {
+
+        FavoriteButton(isSet: .constant(true))
+
+    }
+
+}
